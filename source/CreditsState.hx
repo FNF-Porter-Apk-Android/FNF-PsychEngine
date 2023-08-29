0706ba1b814d67fc5d03e778c22c72dcece46185
FNF-Porter-Apk-Android <143233754+FNF-Porter-Apk-Android@users.noreply.github.com>
2023-08-29 16:16:40 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,6 +81,8 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
+			['Android Port'],
+			['glauber01',               'glauber01',                    'Android Porter',    'https://youtube.com/@glauber01?si=gSw_9dggXGgKmprL',   '5DE7FF',
 			['Psych Engine Android Team'],
 			['MaysLastPlay',		'MaysLastPlay',		'Android Porter',							'https://www.youtube.com/channel/UCx0LxtFR8ROd9sFAq-UxDfw',	'5DE7FF'],
 			['Nuno Filipe Studios',	'nuno',				'Android Porter',							'https://www.youtube.com/channel/UCq7G3p4msVN5SX2CpJ86tTw',	'989c99'],
